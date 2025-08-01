de1b2096ad9a6626b0a6edffb7bb6d84c7edf38d
Volodymyr Honcharov <ladimirvm87@gmail.com>
2025-08-01 18:33:44 +0300

    Form handling: validation rules

--- app/Livewire/Projects/FormModal.php
+++ app/Livewire/Projects/FormModal.php
@@ -2,10 +2,33 @@
 
 namespace App\Livewire\Projects;
 
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class FormModal extends Component
 {
+    #[Validate('required|string|min:2|max:100')]
+    public $name = null;
+
+    #[Validate('required|string|min:2|max:255')]
+    public $description = null;
+
+    #[Validate('required|string')]
+    public $deadline = null;
+
+    #[Validate('required|string|in:pending,in-progress,completed,cancelled')]
+    public $status = 'pending';
+
+    #[Validate('nullable|image|max:5120')]
+    public $project_logo = null;
+
+    public function saveProject()
+    {
+        $validatedProjectRequest = $this->validate();
+
+
+    }
+
     public function render()
     {
         return view('livewire.projects.form-modal');
--- resources/views/livewire/projects/form-modal.blade.php
+++ resources/views/livewire/projects/form-modal.blade.php
@@ -1,51 +1,50 @@
-<div>
-    <flux:modal name="project-modal" class="md:w-[32rem]">
-        <div class="space-y-6">
-            <div>
-                <flux:heading class="font-bold" size="lg">Create Project</flux:heading>
-                <flux:text class="mt-2">Add a new project using the form below.</flux:text>
-            </div>
-
-            {{-- Project name --}}
-            <div class="form-group">
-                <flux:input label="Project Name" placeholder="Enter project name" />
-            </div>
-
-            {{-- Project description --}}
-            <div class="form-group">
-                <flux:textarea label="Description" placeholder="Short project description" rows="3" />
-            </div>
-
-            {{-- Deadline --}}
-            <div class="form-group">
-                <flux:input label="Deadline" type="date" />
-            </div>
-
-            {{-- Status --}}
-            <div class="form-group">
-                <flux:select wire:model="status" label="Status" placeholder="Select status...">
-                    <flux:select.option value="pending">Pending</flux:select.option>
-                    <flux:select.option value="in-progress">In-Progress</flux:select.option>
-                    <flux:select.option value="completed">Completed</flux:select.option>
-                    <flux:select.option value="cancelled">Cancelled</flux:select.option>
-                </flux:select>
-            </div>
-
-            {{-- Project logo --}}
-            <div class="form-group">
-                <flux:input type="file" label="Project Logo" accept="image/*" class="cursor-pointer" />
-            </div>
-
-            {{-- BUttons --}}
-            <div class="flex justify-end pt-4">
-                <flux:spacer />
-
-                <flux:modal.close>
-                    <flux:button variant="ghost" class="mr-2 cursor-pointer">Cancel</flux:button>
-                </flux:modal.close>
-
-                <flux:button type="submit" variant="primary" color="indigo" class="cursor-pointer">Save Project</flux:button>
-            </div>
+<flux:modal name="project-modal" class="md:w-[32rem]">
+    <form wire:submit="saveProject" class="space-y-6">
+        <div>
+            <flux:heading class="font-bold" size="lg">Create Project</flux:heading>
+            <flux:text class="mt-2">Add a new project using the form below.</flux:text>
         </div>
-    </flux:modal>
-</div>
+
+        {{-- Project name --}}
+        <div class="form-group">
+            <flux:input wire:model="name" label="Project Name" placeholder="Enter project name" />
+        </div>
+
+        {{-- Project description --}}
+        <div class="form-group">
+            <flux:textarea wire:model="description" label="Description" placeholder="Short project description" rows="3" />
+        </div>
+
+        {{-- Deadline --}}
+        <div class="form-group">
+            <flux:input wire:model="deadline" label="Deadline" type="date" />
+        </div>
+
+        {{-- Status --}}
+        <div class="form-group">
+            <flux:select wire:model="status" label="Status" placeholder="Select status...">
+                <flux:select.option value="pending">Pending</flux:select.option>
+                <flux:select.option value="in-progress">In-Progress</flux:select.option>
+                <flux:select.option value="completed">Completed</flux:select.option>
+                <flux:select.option value="cancelled">Cancelled</flux:select.option>
+            </flux:select>
+        </div>
+
+        {{-- Project logo --}}
+        <div class="form-group">
+            <flux:input wire:model="project_logo" type="file" label="Project Logo" accept="image/*" class="cursor-pointer" />
+        </div>
+
+        {{-- BUttons --}}
+        <div class="flex justify-end pt-4">
+            <flux:spacer />
+
+            <flux:modal.close>
+                <flux:button variant="ghost" class="mr-2 cursor-pointer">Cancel</flux:button>
+            </flux:modal.close>
+
+            <flux:button type="submit" variant="primary" color="indigo" class="cursor-pointer">Save Project</flux:button>
+        </div>
+    </form>
+</flux:modal>
+
